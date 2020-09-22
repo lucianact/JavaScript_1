dddce285cbcdb7f3b30b27d1f568d6af6062ee83
Luciana Terrazas <luciana.cterrazas@gmail.com>
2020-09-22 19:28:15 +0000

    completed: has_balanced_parens

--- trials.py
+++ trials.py
@@ -98,8 +98,21 @@ def truncate(string):
 
 
 def has_balanced_parens(string):
-    pass  # TODO: replace this line with your code
+    parens = 0
 
+    for char in string:
+        if char == '(':
+            parens += 1
+        elif char == ')':
+            parens -= 1
+
+            if parens > 0:
+                return False
+
+    return parens < 0
+
+#print(has_balanced_parens('((This) (is) (good))'))
+   
 
 def compress(string):
     pass  # TODO: replace this line with your code
